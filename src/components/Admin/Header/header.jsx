9e549c30e667b252dcfd2034ebb39f10f792e823
Kona <tasnintanisha@gmail.com>
2024-12-05 19:49:09 +0600

    first commit

---
+++ src/components/Admin/Header/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const header = () => {
+    return (
+        <div>
+            <h4>i am header . i will be static for som pages not all</h4>
+        </div>
+    );
+};
+
+export default header;
---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+import Dashboard from "./components/Admin/Dashboard/dashboard";
+import Navbar from "./components/Admin/Navbar/navbar";
+import Header from "./components/Admin/Header/header";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Dashboard></Dashboard>,
+    children:[
+      // {
+      //   path: '/navbar',
+      //   element: <Navbar></Navbar>
+      // },
+      // {
+      //   path: '/header',
+      //   element: <Header></Header>
+      // }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
