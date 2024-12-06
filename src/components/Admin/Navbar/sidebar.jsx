313741df39e0c443bfcc858e1e59c1ac61dabfbb
Kona <tasnintanisha@gmail.com>
2024-12-06 17:07:53 +0600

    modify

--- src/components/Admin/Navbar/sidebar.jsx
+++ src/components/Admin/Navbar/sidebar.jsx
@@ -8,8 +8,10 @@ const Sidebar = () => {
       <div className="logo">
         <h2>
           Medi<span className="highlight">Touch</span>
+          <br />
         </h2>
       </div>
+      <div className="menu-section"> 
       <ul className="menu">
         <li className="menu-item">
           <i className="fa fa-home"></i> <span>Home</span>
@@ -36,8 +38,26 @@ const Sidebar = () => {
         <li className="menu-item">
           <i className="fa fa-users"></i> <span>Emergency Doctors</span>
         </li>
+        <li className="menu-item">
+          <i className="fa fa-user-nurse"></i> <span>Add Nurses</span>
+        </li>
+        <li className="menu-item">
+          <i className="fa fa-user"></i> <span>Nurses</span>
+        </li>
+        <li className="menu-item">
+          <i className="fa fa-user-tie"></i> <span>Add Agent</span>
+        </li>
+        <li className="menu-item">
+          <i className="fa fa-users"></i> <span>Agent</span>
+        </li>
       </ul>
+      </div>
+      <br />
+      <div className="logout">
       <button className="logout-button">sign out</button>
+      </div>
+      <br /><br />
+      
     </div>
   );
 };
