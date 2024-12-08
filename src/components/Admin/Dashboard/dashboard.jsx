21156b2a55924c982e4a1acd1274d1c7d115f689
Md Musfiqur Rahman <musfiqurm661@gmail.com>
2024-12-09 00:48:27 +0600

    initial login page added

--- src/components/Admin/Dashboard/dashboard.jsx
+++ src/components/Admin/Dashboard/dashboard.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Navbar from '../Navbar/navbar';
 
 const dashboard = () => {
     return (
         <div>
             <h3>I am admin dashboard page but empty </h3>
 
-            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );
